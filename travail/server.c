38d9f0afd6250f0e77a68c9df12396f50cb9d7e3
Matisse Guilbert <matisse.guilbert31@gmail.com>
2026-09-22 16:15:03 +0200

    Réalisation de la tache Req1.2

--- travail/server.c
+++ travail/server.c
@@ -27,14 +27,14 @@ void echo_server(int sockfd) {
 	}
 }
 
-int handle_bind() {
+int handle_bind(char* port) {
 	struct addrinfo hints, *result, *rp;
 	int sfd;
 	memset(&hints, 0, sizeof(struct addrinfo));
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
-	if (getaddrinfo(NULL, SERV_PORT, &hints, &result) != 0) {
+	if (getaddrinfo(NULL, port, &hints, &result) != 0) {
 		perror("getaddrinfo()");
 		exit(EXIT_FAILURE);
 	}
@@ -57,11 +57,11 @@ int handle_bind() {
 	return sfd;
 }
 
-int main() {
+int main(int argc, char* argv[]) {
 	struct sockaddr cli;
 	int sfd, connfd;
 	socklen_t len;
-	sfd = handle_bind();
+	sfd = handle_bind(argv[1]);
 	if ((listen(sfd, SOMAXCONN)) != 0) {
 		perror("listen()\n");
 		exit(EXIT_FAILURE);
